5f678c113870b14976d4a0aa5acf0e0b7188003f
Andrés Fernández <andresfera99@gmail.com>
2022-05-30 17:11:53 +0200

    commit

---
+++ src/test/java/com/example/jdbcguia/JdbcGuiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jdbcguia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcGuiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
